a5f278942ec67f041a9784c25441fda258f8e491
Daniel Matilla Pozuelo <dmatilla@izertis.com>
2024-03-04 18:18:14 +0100

    test

--- src/js-foundation/06-promises.ts
+++ src/js-foundation/06-promises.ts
@@ -4,8 +4,4 @@ export const getPokemonById = async (id: string | number): Promise<string> => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const pokemon = await http.get(url);
     return pokemon.name;
-    // try {
-    // } catch (error) {
-    //     throw new Error(`Pockemon not found with id: ${id}`);
-    // }
 };
