2e308a9223d1c698c905cfcd4b905fe281c01121
dxmxp <danimatilla@outlook.es>
2024-03-09 00:23:17 +0100

    section-11

--- src/plugins/envs.service.ts
+++ src/plugins/envs.service.ts
@@ -4,8 +4,15 @@ import * as env from 'env-var';
 export const envs = {
     PORT: env.get('PORT').required().asPortNumber(),
     PROD: env.get('PROD').required().asBool(),
+
     // MAILER
     MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
     MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
     MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
+
+    // MONGO
+    MONGO_URL: env.get('MONGO_URL').required().asString(),
+    MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
+    MONGO_USER: env.get('MONGO_USER').required().asString(),
+    MONGO_PASS: env.get('MONGO_PASS').required().asString(),
 }
